e958a0299f6309105ece2e424ca5665752d720ad
afydoz <marat210499@yandex.ru>
2016-12-26 23:53:35 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
-import json
-
+# -*- coding: utf-8 -*-
+import json, pprint
+"""
+Модуль pprint позволяет выводить
+содержимое файла в удобном формате
+"""
 
 def load_data(filepath):
-    pass
 
+    with open(filepath, 'r', encoding='utf-8') as bars:
+        return json.load(bars)
 
 def pretty_print_json(data):
-    pass
-
+    #Вывод содержимого файла в удобном формате
+    pprint.pprint(data)
 
 if __name__ == '__main__':
-    pass
+
+    data = load_data(input("Укажите имя файла:"))
+    pretty_print_json(data)
